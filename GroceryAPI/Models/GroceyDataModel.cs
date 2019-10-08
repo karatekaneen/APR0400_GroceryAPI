2ca5d97a193b7cb6c6b36280cae7e2b320a1b1dd
Karatekaneen <bauhn_@hotmail.com>
2019-10-08 15:20:50 +0200

    Added EF Model and Products controller

---
+++ GroceryAPI/Models/GroceyDataModel.cs
@@ -0,0 +1,66 @@
+namespace GroceryAPI
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class GroceyDataModel : DbContext
+    {
+        public GroceyDataModel()
+            : base("name=ModelGroceyData")
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Staff> Staff { get; set; }
+        public virtual DbSet<Transaction> Transaction { get; set; }
+        public virtual DbSet<TransactionComponent> TransactionComponent { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Category)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Staff>()
+                .Property(e => e.FirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Staff>()
+                .Property(e => e.LastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Staff>()
+                .Property(e => e.SocialSecurityNumber)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Staff>()
+                .HasMany(e => e.Transaction)
+                .WithRequired(e => e.Staff)
+                .HasForeignKey(e => e.StaffMember)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Transaction>()
+                .Property(e => e.PaymentMethod)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Transaction>()
+                .HasMany(e => e.TransactionComponent)
+                .WithRequired(e => e.Transaction)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<TransactionComponent>()
+                .Property(e => e.ProductName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<TransactionComponent>()
+                .Property(e => e.ProductCategory)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ GroceryAPI/Models/TransactionComponent.cs
@@ -0,0 +1,30 @@
+namespace GroceryAPI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TransactionComponent")]
+    public partial class TransactionComponent
+    {
+        public int Id { get; set; }
+
+        public double Quantity { get; set; }
+
+        public int TransactionId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ProductName { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ProductCategory { get; set; }
+
+        public virtual Transaction Transaction { get; set; }
+    }
+}
